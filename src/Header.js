13b48fc13ea97ddf7429b15e357f27dcc38fdc5c
DMcDe <dmcde@umich.edu>
2024-07-01 19:02:40 -0500

    Added nav functionality to header

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,21 @@
+import {useNavigate} from 'react-router-dom';
+
 const Header = () => {
+    const nav = useNavigate();
+
+    const navHome = () => nav("/");
+
+    const navExp = () => nav("/experience");
+
+    const navWSA = () => window.open("https://www.wolverinesportsanalytics.com/blog", "mozillaTab");
+
     return(
         <header className="Header">
-            <h1 className="header-name">DAVID MCDERMOTT</h1>
+            <h1 className="header-name" onClick={navHome}>DAVID MCDERMOTT</h1>
             <div className="header-menu">
-                <div className="header-button">ABOUT</div>
-                <div className="header-button">EXPERIENCE</div>
-                <div className="header-button">WRITING</div>
+                <div className="header-button" onClick={navHome}>ABOUT</div>
+                <div className="header-button" onClick={navExp}>EXPERIENCE</div>
+                <div className="header-button" onClick={navWSA}>WRITING</div>
                 <div className="header-button">LINKS</div>
             </div>
         </header>
