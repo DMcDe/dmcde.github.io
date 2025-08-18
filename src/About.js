f1e8da518622fe00f1037054916ecfc35af54758
DMcDe <dmcde@umich.edu>
2025-08-18 19:43:26 -0400

    Update experiences and About, August 2025

--- src/About.js
+++ src/About.js
@@ -9,13 +9,13 @@ const About = () => {
             <div className="about-text">
                 <p style={{fontSize: "1.5rem", fontWeight: "bold"}}>Welcome!</p>
 
-                <p>I’m David, a junior studying Computer Science and Interdisciplinary Physics with a concentration in Quantum Computing at the University of Michigan.</p>
+                <p>I’m David, a senior studying Computer Science and Interdisciplinary Physics with a concentration in Quantum Computing at the University of Michigan.</p>
             
-                <p>I have experience as a data scientist at Wilson Sporting Goods, a researcher with professors at Michigan and the University of Illinois at Chicago, and a software engineer and computer scientist with Traders at Michigan, Wolverine Sports Analytics, and Kappa Theta Pi.</p>
+                <p>I have experience as a software engineer at Pure Storage, a data scientist at Wilson Sporting Goods, a researcher with professors at Michigan and the University of Illinois at Chicago, and a computer scientist with Traders at Michigan, Wolverine Sports Analytics, and Kappa Theta Pi.</p>
             
-                <p>I’m interested in fields spanning every corner of data and tech: sports analytics, quantitative analysis and trading, emerging technology, academic research, and so much more. I constantly challenge myself to expand my knowledge and skill set.</p>
+                <p>I’m interested in fields spanning every corner of data and technology: systems development and network programming, sports analytics, quantitative analysis and trading, emerging tech, academic research, and so much more. I constantly challenge myself to expand my knowledge and skill set.</p>
             
-                <p>In my free time, I love cooking and baking (including previously as a baker at Crumbl), coaching policy debate, playing poker and spikeball, and listening to music and podcasts.</p>
+                <p>In my free time, I love cooking and baking (including previously as a baker at Crumbl), teaching students as an Instructional Aide in Michigan's EECS 482 (Operating Systems), playing poker and spikeball, and listening to music and podcasts. I also participated in and coached policy debate for 10 years.</p>
             
                 <p>I’d love to hear from you! Contact me at dmcde@umich.edu</p>
             </div>
